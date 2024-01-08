c2d57d29b88fb00a9543167db6aea5db22cf3068
lucas <40892245+1u1ucas@users.noreply.github.com>
2024-01-08 14:52:56 +0100

    V1

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+let heroCount = 2;
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  // title = 'La tour des 1 héros';
+  titleStyle = { 'font-family': 'Arial', 'font-size': '2rem' };
+  username = 'Anonymous';
+  heroes = ['Batman', 'Superman', 'Wonderwoman', 'Ironman'];
+  heroToAdd = '';
+
+  addHero() {
+    this.heroes.push(this.heroToAdd);
+    this.title = 'La tour des ' + this.heroes.length + ' héros';
+  }
+}
